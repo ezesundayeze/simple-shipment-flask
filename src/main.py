a8d644a757e11e60d9d905e885e0b4d47b182f29
Eze Sunday Eze <mailsforeze@gmail.com>
2021-11-16 16:01:16 +0100

    update  app port

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from app_config import app
+from dotenv import load_dotenv
 from model import db
+load_dotenv()
+port= os.getenv('PORT')
+
 if __name__ == '__main__':
     db.init_app(app)
-    app.run(debug=True)
+    app.run(debug=True, port={port})
